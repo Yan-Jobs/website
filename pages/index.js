afb8a9e095f24715ad90ac59f031acde1b6551a9
Yan <yan@yanjobs.me>
2022-09-09 14:37:06 +0200

    🤦‍♂️ That's why I shouldnt code on the Notes iPad app

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ export default function LearnMore() {
       <Head>
         <title>Yan | Home</title>
         <link rel="apple-touch-icon" sizes="180x180" href="../public/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon-32x32.png /">
-        <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon-16x16.png /">
-        <link rel="manifest" href="../public/site.webmanifest"/ >
+        <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon-16x16.png" />
+        <link rel="manifest" href="../public/site.webmanifest" />
         <link rel="mask-icon" href="../public/safari-pinned-tab.svg" color="#0b272f" />
         <link rel="shortcut icon" href="../public/favicon.ico" />
         <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="msapplication-config" content="../public/browserconfig.xml /">
+        <meta name="msapplication-config" content="../public/browserconfig.xml" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <main className="min-h-screen p-16 flex-1 flex flex-col justify-center items-center">
